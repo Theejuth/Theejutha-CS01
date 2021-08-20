f4db68e441f5f7a424a6bb686c9a9a399617da55
Theejuth <86812783+Theejuth@users.noreply.github.com>
2021-08-20 09:35:26 +0700

    Create CS01-15.py

---
+++ CS01-15.py
@@ -0,0 +1,8 @@
+def my_list(list):
+ for i in range(len(thislist)):
+    print(i)
+    if (thislist[i] == 20):
+           thislist[i] = 200
+    print(thislist)
+thislist = [5,10,15,20,25,50,20]
+my_list(thislist)
