c6db4b8d2dbfd0a938cc686e30fdb1d8733ffcab
Theejuth <86812783+Theejuth@users.noreply.github.com>
2021-09-26 13:44:40 +0700

    Create CS01-Grading.py

---
+++ CS01-Grading.py
@@ -0,0 +1,15 @@
+x = int(input("Enter your score :"))
+if ( 100 >= x >= 80):
+    print("Grade A")
+elif (100 >= x >= 75):
+    print("Grade B")
+elif (100 >= x >= 70):
+    print("Grade C")
+elif (100 >= x >= 65):
+    print("Grade D")
+elif (100 >= x >= 60):
+    print("Grade F")
+elif (x > 100):
+    print("Error")
+else:
+    print("Error")
