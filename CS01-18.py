5da88aeba243df0a5cd3e6ed87362a3336c2754c
Theejuth <86812783+Theejuth@users.noreply.github.com>
2021-10-01 08:57:31 +0700

    Update CS01-18.py

--- CS01-18.py
+++ CS01-18.py
@@ -5,4 +5,4 @@ myText = Label(text = "My name is",fg = "blue",font = 20).grid(row = 0, column =
 myText = Label(text = "Theejutha",fg = "green",font = 20).grid(row = 1, column = 1)
 myText = Label(text = "Konsombat",fg = "red",font = 20).grid(row = 2, column = 2)
 root.geometry("300x300")
-root.mainloop
+root.mainloop()
